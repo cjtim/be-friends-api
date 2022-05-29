5f93a53260d959273700e2fb75cf3c0fbb219025
cjtim <47479090+cjtim@users.noreply.github.com>
2022-05-29 23:58:11 +0700

    fix create user logic

--- internal/pkg/line/db.go
+++ internal/pkg/line/db.go
@@ -9,16 +9,16 @@ import (
 func (p *LineProfile) CreateLineUser() error {
 	ctx := context.Background()
 
-	_, err := r.DB.LineUsers.FindFirst(
+	lineUser, err := r.DB.LineUsers.FindFirst(
 		r.LineUsers.LineUId.Equals(p.LineUid),
 	).Exec(ctx)
 	newUser := err == r.ErrNotFound
 	if err != nil && !newUser {
 		return err
 	}
+
 	var createdUser *r.UsersModel
 	if newUser {
-		// BUG new user always create at login
 		createdUser, err = r.DB.Users.CreateOne(
 			r.Users.Name.Set(p.Name),
 			r.Users.LoginMethodID.Set(2),
@@ -26,15 +26,27 @@ func (p *LineProfile) CreateLineUser() error {
 		if err != nil {
 			return err
 		}
+
+		_, err = r.DB.LineUsers.CreateOne(
+			r.LineUsers.LineUId.Set(p.LineUid),
+			r.LineUsers.Name.Set(p.Name),
+			r.LineUsers.User.Link(
+				r.Users.ID.Equals(createdUser.ID),
+			),
+			r.LineUsers.ProfilePic.Set(p.Picture),
+		).Exec(ctx)
+		return err
 	}
 
+	// Old user
+	// Update profilePic
 	_, err = r.DB.LineUsers.UpsertOne(
 		r.LineUsers.LineUId.Equals(p.LineUid),
 	).Create(
 		r.LineUsers.LineUId.Set(p.LineUid),
 		r.LineUsers.Name.Set(p.Name),
 		r.LineUsers.User.Link(
-			r.Users.ID.Equals(createdUser.ID),
+			r.Users.ID.Equals(lineUser.UserID),
 		),
 		r.LineUsers.ProfilePic.Set(p.Picture),
 	).Update(
