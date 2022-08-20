ed4294dc526483b8ddc79a7d9824ba5692bfcd29
cjtim <iam@cjtim.com>
2022-08-20 18:21:43 +0700

    add handler

--- handlers/pet/create.go
+++ handlers/pet/create.go
@@ -1,15 +1,33 @@
 package pet
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/cjtim/be-friends-api/repository"
+	"github.com/gofiber/fiber/v2"
+)
 
 type createBody struct {
 	Name        string  `json:"name"`
 	Description *string `json:"description"`
-	lat         int     `json:"lat"`
-	lng         int     `json:"lng"`
-	images      []byte  `json:"images"`
+	Lat         int     `json:"lat"`
+	Lng         int     `json:"lng"`
+	Images      []byte  `json:"images"`
 }
 
 func PetCreate(c *fiber.Ctx) error {
+	body := createBody{}
+	err := c.BodyParser(&body)
+	if err != nil {
+		return err
+	}
+	err = repository.PetRepo.Create(repository.Pet{
+		Name:        body.Name,
+		Description: body.Description,
+		Lat:         float64(body.Lat),
+		Lng:         float64(body.Lng),
+	})
 
+	if err != nil {
+		return err
+	}
+	return c.SendStatus(fiber.StatusCreated)
 }
