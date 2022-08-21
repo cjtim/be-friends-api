12950f3cbf68ea7fbbc10f11deba3d38cecd445d
cjtim <iam@cjtim.com>
2022-08-21 21:14:16 +0700

    feat: new shelter, campaign, donate tables

--- tools/db/1-init.sql
+++ tools/db/1-init.sql
@@ -1,23 +1,96 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+------------------------------
+-- Shelter
+------------------------------
+CREATE TABLE "shelter" (
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255),
+    "password" VARCHAR(255),    
+    "picture_url" text,
+    "description" text,
+    lat NUMERIC(15,10) NOT NULL,
+    lng NUMERIC(15,10) NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT NOW(),
+    "updated_at" timestamp NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "PK_shelter" PRIMARY KEY ("id"),
+    CONSTRAINT "CS_shelter__email" UNIQUE ("email")
+);
+-- Index for search by id
+CREATE INDEX IDX01_id_shelter ON "shelter" (id);
+CREATE INDEX IDX02_name_shelter ON "shelter" (name);
+
+------------------------------
+-- User
+------------------------------
 CREATE TABLE "user" (
     "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "name" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255),
-    "password" VARCHAR(255),
     "line_uid" VARCHAR(255),
+    "shelter_id" uuid,
     "picture_url" text,
-    "is_org"  BOOL,
-    "is_admin" BOOL DEFAULT FALSE NOT NULL, 
+    "is_admin" BOOL DEFAULT FALSE NOT NULL,
     "created_at" timestamp NOT NULL DEFAULT NOW(),
     "updated_at" timestamp NOT NULL DEFAULT NOW(),
 
-    CONSTRAINT "PK_user_id" PRIMARY KEY ("id"),
-    CONSTRAINT "CS_user_email" UNIQUE ("email"),
-    CONSTRAINT "CS_user_line_uid" UNIQUE ("line_uid")
+    CONSTRAINT "PK_user" PRIMARY KEY ("id"),
+    CONSTRAINT "CS_user__email" UNIQUE ("email"),
+    CONSTRAINT "CS_user__line_uid" UNIQUE ("line_uid"),
+    CONSTRAINT "FK_user__shelter" FOREIGN KEY (shelter_id) REFERENCES "shelter" ("id"),
+    CONSTRAINT "CS_user__shelter_unique" UNIQUE ("shelter_id")
 );
 -- Index for search by id
 CREATE INDEX IDX01_id_user ON "user" (id);
+CREATE INDEX IDX02_line_uid_user ON "user" (line_uid);
+CREATE INDEX IDX03_shelter_id_user ON "user" (shelter_id);
+
+------------------------------
+-- Campaign
+------------------------------
+CREATE TABLE "campaign" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "picture_url" text,
+    "description" text,
+    "min_amount" double precision NOT NULL,
+    "shelter_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT NOW(),
+    "updated_at" timestamp NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "PK_campaign" PRIMARY KEY ("id"),
+    CONSTRAINT "CS_campaign__name" UNIQUE ("name"),
+    CONSTRAINT "FK_campaign__shelter" FOREIGN KEY (shelter_id) REFERENCES "shelter" ("id"),
+    CONSTRAINT "CS_campaign__shelter_unique" UNIQUE ("shelter_id")
+);
+-- Index for search by id
+CREATE INDEX IDX01_id_campaign ON "campaign" (id);
+CREATE INDEX IDX02_name_campaign ON "campaign" (name);
+CREATE INDEX IDX03_shelter_id_campaign ON "campaign" (shelter_id);
+
+------------------------------
+-- donate
+------------------------------
+CREATE TABLE "donate" (
+    "id" SERIAL NOT NULL,
+    "picture_url" text NOT NULL,
+    "description" text,
+    "amount" double precision NOT NULL,
+    "campaign_id" SERIAL NOT NULL,
+    "user_id" uuid NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT NOW(),
+    "updated_at" timestamp NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "PK_donate" PRIMARY KEY ("id"),
+    CONSTRAINT "FK_donate__campaign" FOREIGN KEY (campaign_id) REFERENCES "campaign" ("id"),
+    CONSTRAINT "FK_donate__user" FOREIGN KEY (user_id) REFERENCES "user" ("id"),
+    CONSTRAINT "CS_donate__user_unique" UNIQUE ("user_id")
+);
+-- Index for search by id
+CREATE INDEX IDX01_id_donate ON "donate" (id);
+CREATE INDEX IDX02_user_id_donate ON "donate" (user_id);
 
 ------------------------------
 -- Tag table
@@ -30,8 +103,8 @@ CREATE TABLE "tag" (
     "created_at" timestamp NOT NULL DEFAULT NOW(),
     "updated_at" timestamp NOT NULL DEFAULT NOW(),
 
-    CONSTRAINT "PK_tag_id" PRIMARY KEY ("id"),
-    CONSTRAINT "CS_tag_name" UNIQUE ("name")
+    CONSTRAINT "PK_tag" PRIMARY KEY ("id"),
+    CONSTRAINT "CS_tag__name" UNIQUE ("name")
 );
 -- Search by id and name
 CREATE INDEX IDX01_id_tag ON "tag" (id);
@@ -47,10 +120,12 @@ CREATE TABLE "pet" (
     "description" VARCHAR(255),
     "created_at" timestamp NOT NULL DEFAULT NOW(),
     "updated_at" timestamp NOT NULL DEFAULT NOW(),
+    "user_id" uuid NOT NULL,
     lat NUMERIC(15,10) NOT NULL,
     lng NUMERIC(15,10) NOT NULL,
 
-    CONSTRAINT "PK_pet_id" PRIMARY KEY ("id")
+    CONSTRAINT "PK_pet" PRIMARY KEY ("id"),
+    CONSTRAINT "FK_pet__user" FOREIGN KEY (user_id) REFERENCES "user" ("id")
 );
 -- Search by id and name
 CREATE INDEX IDX01_id_pet ON "pet" (id);
@@ -64,9 +139,9 @@ CREATE TABLE "tag_pet" (
     pet_id SERIAL NOT NULL,
     tag_id SERIAL NOT NULL,
 
-    CONSTRAINT "PK_pet_tag_id" PRIMARY KEY (pet_id, tag_id),
-    CONSTRAINT "FK_pet" FOREIGN KEY (pet_id) REFERENCES "pet" ("id"),
-    CONSTRAINT "FK_tag" FOREIGN KEY (tag_id) REFERENCES "tag" ("id")
+    CONSTRAINT "PK_tag_pet" PRIMARY KEY (pet_id, tag_id),
+    CONSTRAINT "FK_tag_pet__pet" FOREIGN KEY (pet_id) REFERENCES "pet" ("id"),
+    CONSTRAINT "FK_tag_pet__tag" FOREIGN KEY (tag_id) REFERENCES "tag" ("id")
 );
 
 ------------------------------
@@ -77,6 +152,6 @@ CREATE TABLE "pic_pet" (
     pet_id SERIAL NOT NULL,
     picture_url TEXT NOT NULL,
 
-    CONSTRAINT "PK_pet_id_picture_url" PRIMARY KEY (pet_id, picture_url),
-    CONSTRAINT "FK_pet" FOREIGN KEY (pet_id) REFERENCES "pet" ("id")
+    CONSTRAINT "PK_pic_pet" PRIMARY KEY (pet_id, picture_url),
+    CONSTRAINT "FK_pic_pet__pet" FOREIGN KEY (pet_id) REFERENCES "pet" ("id")
 );
