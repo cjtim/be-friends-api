a195e2237bbaab4a466ff5af23a33a93fe13fcaa
cjtim <47479090+cjtim@users.noreply.github.com>
2022-10-11 17:22:32 +0000

    feat: shelter able to fetch users interested by pet_id

--- handlers/interest/list.go
+++ handlers/interest/list.go
@@ -1,6 +1,8 @@
 package interest
 
 import (
+	"strconv"
+
 	"github.com/cjtim/be-friends-api/internal/auth"
 	"github.com/cjtim/be-friends-api/repository"
 	"github.com/gofiber/fiber/v2"
@@ -11,9 +13,24 @@ func List(c *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
-	interested, err := repository.InterestedRepo.ListByUserID(claims.ID)
+	petIdStr := c.Query("pet_id")
+	if petIdStr == "" {
+		// List intested by user_id
+		interested, err := repository.InterestedRepo.ListByUserID(claims.ID)
+		if err != nil {
+			return err
+		}
+		return c.JSON(interested)
+	}
+	// If pet_id exist
+	// Fetch users that interested
+	petId, err := strconv.Atoi(petIdStr)
 	if err != nil {
 		return err
 	}
-	return c.JSON(interested)
+	users, err := repository.InterestedRepo.ListByPetID(petId)
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
 }
--- repository/interested.go
+++ repository/interested.go
@@ -19,6 +19,15 @@ func (i *InterestedImpl) ListByUserID(userID uuid.UUID) (output []Interested, er
 	return
 }
 
+func (i *InterestedImpl) ListByPetID(petId int) (output []User, err error) {
+	stm := `SELECT u.* FROM "interested" l 
+			INNER JOIN "user" u 
+			ON u.id = l.user_id 
+			WHERE l.pet_id = $1`
+	err = DB.Select(&output, stm, petId)
+	return
+}
+
 func (i *InterestedImpl) Add(petID int, userID uuid.UUID) error {
 	stm := `INSERT INTO "interested" (pet_id, user_id) VALUES ($1, $2)`
 	_, err := DB.Exec(stm, petID, userID)
